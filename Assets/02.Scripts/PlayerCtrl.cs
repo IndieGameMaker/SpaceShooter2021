d4e6adfd9b75fff97060a1ea51b031726c5e4795
Lee Jae Hyun <myleje@gmail.com>
2021-05-28 14:56:20 +0900

    4-7

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -7,6 +7,9 @@ public class PlayerCtrl : MonoBehaviour
     // 컴포넌트를 캐시 처리할 변수
     private Transform tr;
 
+    // 이동 속도 변수 (public으로 선언되어 인스펙터 뷰에 노출됨)
+    public float moveSpeed = 10.0f;
+
     void Start()
     {
         // Transform 컴포넌트를 추출해 변수에 대입
@@ -28,6 +31,6 @@ public class PlayerCtrl : MonoBehaviour
         // tr.position += Vector3.forward * 1;
 
         // Translate 함수를 사용한 이동 로직
-        tr.Translate(Vector3.forward * 1);
+        tr.Translate(Vector3.forward * Time.deltaTime * v * moveSpeed);
     }
 }
