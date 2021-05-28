baa38f436a9ad85640afb5d8b6625e35bf7af4d3
Lee Jae Hyun <myleje@gmail.com>
2021-05-28 14:59:36 +0900

    4-10

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -7,27 +7,34 @@ public class PlayerCtrl : MonoBehaviour
     // 컴포넌트를 캐시 처리할 변수
     private Transform tr;
 
-    // 이동 속도 변수 (public으로 선언되어 인스펙터 뷰에 노출됨)
+    // 이동 속력 변수 (public으로 선언되어 인스펙터 뷰에 노출됨)
     public float moveSpeed = 10.0f;
+
+    // 회전 속도 변수
+    public float turnSpeed = 80.0f;
 
     void Start()
     {
-        // Transform 컴포넌트를 추출해 변수에 대입
+        // 컴포넌트를 추출해 변수에 대입
         tr = GetComponent<Transform>();
     }
 
     void Update()
     {
-        float h = Input.GetAxis("Horizontal"); // -1.0f ~ 0.0f ~ +1.0f
-        float v = Input.GetAxis("Vertical"); // -1.0f ~ 0.0f ~ +1.0f
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        float r = Input.GetAxis("Mouse X");
 
-        Debug.Log("h=" + h);
-        Debug.Log("v=" + v);
+        // Debug.Log("h=" + h);
+        // Debug.Log("v=" + v);
 
         // 전후좌우 이동 방향 벡터 계산
         Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
 
         // Translate(이동 방향 * 속력 * Time.deltaTime)
         tr.Translate(moveDir.normalized * moveSpeed * Time.deltaTime);
+
+        // Vector3.up 축을 기준으로 turnSpeed만큼의 속도로 회전
+        tr.Rotate(Vector3.up * turnSpeed * Time.deltaTime * r);
     }
 }
