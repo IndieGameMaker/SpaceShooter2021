04baef4b2621a21087a76113b78b4732781c9bb8
Lee Jae Hyun <myleje@gmail.com>
2021-05-29 17:59:38 +0900

    5-2 RemoveBullet - 충돌 콜백 함수 작성

---
+++ Assets/02.Scripts/RemoveBullet.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveBullet : MonoBehaviour
+{
+    // 충돌이 시작할 때 발생하는 이벤트
+    void OnCollisionEnter(Collision coll)
+    {
+        // 충돌한 게임오브젝트의 태그값 비교
+        if (coll.collider.tag == "BULLET")
+        {
+            // 충돌한 게임오브젝트 삭제
+            Destroy(coll.gameObject);
+        }
+    }
+}
