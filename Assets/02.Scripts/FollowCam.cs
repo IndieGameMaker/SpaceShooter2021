ad57542b1aa90689beae2e70b2ebed8b250a5953
Lee Jae Hyun <myleje@gmail.com>
2021-05-29 14:29:00 +0900

    [스크립트 4-14] FollowCam - Slerp 함수를 활용해 부드럽게 이동하도록 처리

--- Assets/02.Scripts/FollowCam.cs
+++ Assets/02.Scripts/FollowCam.cs
@@ -17,6 +17,9 @@ public class FollowCam : MonoBehaviour
     [Range(0.0f, 10.0f)]
     public float height = 2.0f;
 
+    // 반응 속도
+    public float damping = 10.0f;
+
     void Start()
     {
         // Main Camera 자신의 Transform 컴포넌트를 추출
@@ -27,9 +30,14 @@ public class FollowCam : MonoBehaviour
     {
         // 추적해야 할 대상의 뒤쪽으로 distance만큼 이동
         // 높이를 height만큼 이동
-        camTr.position = targetTr.position
-                         + (-targetTr.forward * distance)
-                         + (Vector3.up * height);
+        Vector3 pos = targetTr.position
+                      + (-targetTr.forward * distance)
+                      + (Vector3.up * height);
+
+        // 구면 선형 보간 함수를 사용해 부드럽게 위치를 변경
+        camTr.position = Vector3.Slerp(camTr.position,            // 시작 위치
+                                       pos,                       // 목표 위치
+                                       Time.deltaTime * damping); // 시간 t
 
         // Camera를 피벗 좌표를 향해 회전
         camTr.LookAt(targetTr.position);
