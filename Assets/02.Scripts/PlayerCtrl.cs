d31dd4a523ff777ac8b67b61c80342af203050c9
Lee Jae Hyun <myleje@gmail.com>
2021-05-29 14:15:14 +0900

    4-12

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -42,5 +42,36 @@ public class PlayerCtrl : MonoBehaviour
 
         // Vector3.up 축을 기준으로 turnSpeed만큼의 속도로 회전
         tr.Rotate(Vector3.up * turnSpeed * Time.deltaTime * r);
+
+        // 주인공 캐릭터의 애니메이션 설정
+        PlayerAnim(h, v);
     }
+
+    void PlayerAnim(float h, float v)
+    {
+        // 키보드 입력값을 기준으로 동작할 애니메이션 수행
+        if (v >= 0.1f)
+        {
+            anim.CrossFade("RunF", 0.25f);  // 전진 애니메이션 실행
+        }
+        else if (v <= -0.1f)
+        {
+            anim.CrossFade("RunB", 0.25f);  // 후진 애니메이션 실행
+        }
+        else if (h >= 0.1f)
+        {
+            anim.CrossFade("RunR", 0.25f);  // 오른쪽 이동 애니메이션 실행
+        }
+        else if (h <= -0.1f)
+        {
+            anim.CrossFade("RunL", 0.25f);  // 왼쪽 이동 애니메이션 실행
+        }
+        else
+        {
+            anim.CrossFade("Idle", 0.25f);   // 정지 시 Idle 애니메이션 실행
+        }
+    }
+
+
+
 }
