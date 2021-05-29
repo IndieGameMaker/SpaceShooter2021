44c46b82e41b5399bb37d593b28bdfe55dc0b5d6
Lee Jae Hyun <myleje@gmail.com>
2021-05-29 18:01:04 +0900

    [스크립트 5-3] RemoveBullet - CompareTag 함수로 변경

--- Assets/02.Scripts/RemoveBullet.cs
+++ Assets/02.Scripts/RemoveBullet.cs
@@ -8,7 +8,7 @@ public class RemoveBullet : MonoBehaviour
     void OnCollisionEnter(Collision coll)
     {
         // 충돌한 게임오브젝트의 태그값 비교
-        if (coll.collider.tag == "BULLET")
+        if (coll.collider.CompareTag("BULLET"))
         {
             // 충돌한 게임오브젝트 삭제
             Destroy(coll.gameObject);
