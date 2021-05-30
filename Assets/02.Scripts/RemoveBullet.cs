1d44c638f3b5f3a991b71a8952f12e0aa7dfc088
Lee Jae Hyun <myleje@gmail.com>
2021-05-30 20:04:59 +0900

    [스크립트 5-6] RemoveBullet - 스파크 효과

--- Assets/02.Scripts/RemoveBullet.cs
+++ Assets/02.Scripts/RemoveBullet.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class RemoveBullet : MonoBehaviour
 {
+    // 스파크 파티클 프리팹을 연결할 변수
+    public GameObject sparkEffect;
+
     // 충돌이 시작할 때 발생하는 이벤트
     void OnCollisionEnter(Collision coll)
     {
         // 충돌한 게임오브젝트의 태그값 비교
         if (coll.collider.CompareTag("BULLET"))
         {
+            // 스파크 파티클을 동적으로 생성
+            Instantiate(sparkEffect, coll.transform.position, Quaternion.identity);
+
             // 충돌한 게임오브젝트 삭제
             Destroy(coll.gameObject);
         }
