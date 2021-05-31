01df19d088484fe5241dd0205b6467ecbc59be6a
Lee Jae Hyun <myleje@gmail.com>
2021-05-31 23:22:04 +0900

    [스크립트 5-15] FireCtrl - MuzzleFlash의 텍스처 오프셋과 크기 및 회전 설정

--- Assets/02.Scripts/FireCtrl.cs
+++ Assets/02.Scripts/FireCtrl.cs
@@ -49,10 +49,25 @@ public class FireCtrl : MonoBehaviour
 
     IEnumerator ShowMuzzleFlash()
     {
+        // 오프셋 좌푯값을 랜덤 함수로 생성
+        Vector2 offset = new Vector2(Random.Range(0, 2), Random.Range(0, 2)) * 0.5f;
+        // 텍스처의 오프셋 값 설정
+        muzzleFlash.material.mainTextureOffset = offset;
+
+        // MuzzleFlash의 회전 변경
+        float angle = Random.Range(0, 360);
+        muzzleFlash.transform.localRotation = Quaternion.Euler(0, 0, angle);
+
+        // MuzzleFlash의 크기 조절
+        float scale = Random.Range(1.0f, 2.0f);
+        muzzleFlash.transform.localScale = Vector3.one * scale;
+
         // MuzzleFlash 활성화
         muzzleFlash.enabled = true;
+
         // 0.2초 동안 대기(정지)하는 동안 메시지 루프로 제어권을 양보
         yield return new WaitForSeconds(0.2f);
+
         // MuzzleFlash 비활성화
         muzzleFlash.enabled = false;
     }
