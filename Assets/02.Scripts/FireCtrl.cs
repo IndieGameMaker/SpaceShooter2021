1fb64be3b3da2884ae1cbee86ce7490d7b300798
Lee Jae Hyun <myleje@gmail.com>
2021-05-31 22:57:23 +0900

    [스크립트 5-12] FireCtrl - 총소리 오디오를 발생시키는 로직

--- Assets/02.Scripts/FireCtrl.cs
+++ Assets/02.Scripts/FireCtrl.cs
@@ -2,16 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 반드시 필요한 컴포넌트를 명시해 해당 컴포넌트가 삭제되는 것을 방지하는 어트리뷰트
+[RequireComponent(typeof(AudioSource))]
 public class FireCtrl : MonoBehaviour
 {
     // 총알 프리팹
     public GameObject bullet;
     // 총알 발사 좌표
     public Transform firePos;
+    // 총소리에 사용할 오디오 음원
+    public AudioClip fireSfx;
+
+    // AudioSource 컴포넌트를 저장할 변수
+    private new AudioSource audio;
 
     void Start()
     {
-
+        audio = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -27,5 +34,7 @@ public class FireCtrl : MonoBehaviour
     {
         // Bullet 프리팹을 동적으로 생성(생성할 객체, 위치, 회전)
         Instantiate(bullet, firePos.position, firePos.rotation);
+        // 총소리 발생
+        audio.PlayOneShot(fireSfx, 1.0f);
     }
 }
