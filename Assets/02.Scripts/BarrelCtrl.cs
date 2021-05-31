497d45120e13af0744ce597bccb3be4843b1e6ac
Lee Jae Hyun <myleje@gmail.com>
2021-05-31 22:45:43 +0900

    [스크립트 5-11] BarrelCtrl - 폭발 반경에 들어와 있는 드럼통을 대상으로 폭발력 전달

--- Assets/02.Scripts/BarrelCtrl.cs
+++ Assets/02.Scripts/BarrelCtrl.cs
@@ -8,6 +8,8 @@ public class BarrelCtrl : MonoBehaviour
     public GameObject expEffect;
     // 무작위로 적용할 텍스처 배열
     public Texture[] textures;
+    // 폭발 반경
+    public float radius = 10.0f;
     // 하위에 있는 Mesh Renderer 컴포넌트를 저장할 변수 
     private new MeshRenderer renderer;
 
@@ -53,11 +55,34 @@ public class BarrelCtrl : MonoBehaviour
         Destroy(exp, 5.0f);
 
         // Rigidbody 컴포넌트의 mass를 1.0으로 수정해 무게를 가볍게 함
-        rb.mass = 1.0f;
+        // rb.mass = 1.0f;
         // 위로 솟구치는 힘을 가함
-        rb.AddForce(Vector3.up * 1500.0f);
+        // rb.AddForce(Vector3.up * 1500.0f);
+
+        // 간접 폭발력 전달
+        IndirectDamage(tr.position);
 
         // 3초 후에 드럼통 제거
         Destroy(gameObject, 3.0f);
     }
+
+    // 폭발력을 주변에 전달하는 함수
+    void IndirectDamage(Vector3 pos)
+    {
+        // 주변에 있는 드럼통을 모두 추출
+        Collider[] colls = Physics.OverlapSphere(pos, radius, 1 << 3);
+
+        foreach (var coll in colls)
+        {
+            // 폭발 범위에 포함된 드럼통의 Rigidbody 컴포넌트 추출
+            rb = coll.GetComponent<Rigidbody>();
+            // 드럼통의 무게를 가볍게 함
+            rb.mass = 1.0f;
+            // freezeRotation 제한값을 해제
+            rb.constraints = RigidbodyConstraints.None;
+            // 폭발력을 전달
+            rb.AddExplosionForce(1500.0f, pos, radius, 1200.0f);
+        }
+    }
+
 }
