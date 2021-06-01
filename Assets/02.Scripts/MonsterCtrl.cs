7b59d1bfd7c6350b44991e4dc6fa0dad2792a67c
Lee Jae Hyun <myleje@gmail.com>
2021-06-01 16:28:58 +0900

    [스크립트 6-9] MonsterCtrl - OnPlayerDie 함수 추가

--- Assets/02.Scripts/MonsterCtrl.cs
+++ Assets/02.Scripts/MonsterCtrl.cs
@@ -35,6 +35,7 @@ public class MonsterCtrl : MonoBehaviour
     private readonly int hashTrace = Animator.StringToHash("IsTrace");
     private readonly int hashAttack = Animator.StringToHash("IsAttack");
     private readonly int hashHit = Animator.StringToHash("Hit");
+    private readonly int hashPlayerDie = Animator.StringToHash("PlayerDie");
 
     // 혈흔 효과 프리팹
     private GameObject bloodEffect;
@@ -177,5 +178,15 @@ public class MonsterCtrl : MonoBehaviour
     void OnTriggerEnter(Collider coll)
     {
         Debug.Log(coll.gameObject.name);
+    }
+
+    void OnPlayerDie()
+    {
+        // 몬스터의 상태를 체크하는 코루틴 함수를 모두 정지시킴
+        StopAllCoroutines();
+
+        // 추적을 정지하고 애니메이션을 수행
+        agent.isStopped = true;
+        anim.SetTrigger(hashPlayerDie);
     }
 }
