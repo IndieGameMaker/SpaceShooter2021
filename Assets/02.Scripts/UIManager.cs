a9c195b5f6af474d9e21b1bc8b945fd6c83519e5
Lee Jae Hyun <myleje@gmail.com>
2021-06-02 14:30:32 +0900

    [스크립트 7-1] UIManager 생성

---
+++ Assets/02.Scripts/UIManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public void OnButtonClick()
+    {
+        Debug.Log("Click Button");
+    }
+}
