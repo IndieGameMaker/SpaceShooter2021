1667e945500ac04d34705fe12f8728c85e20a0ff
Lee Jae Hyun <myleje@gmail.com>
2021-06-02 14:35:59 +0900

    [스크립트 7-2] UIManager - 버튼 이벤트 처리

--- Assets/02.Scripts/UIManager.cs
+++ Assets/02.Scripts/UIManager.cs
@@ -1,11 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;       // Unity-UI를 사용하기 위해 선언한 네임스페이스
+using UnityEngine.Events;   // UnityEvent 관련 API를 사용하기 위해 선언한 네임스페이스
 
 public class UIManager : MonoBehaviour
 {
-    public void OnButtonClick()
+    // 버튼을 연결할 변수
+    public Button startButton;
+    public Button optionButton;
+    public Button shopButton;
+
+    private UnityAction action;
+
+    void Start()
     {
-        Debug.Log("Click Button");
+        // UnityAction을 사용한 이벤트 연결 방식
+        action = () => OnButtonClick(startButton.name);
+        startButton.onClick.AddListener(action);
+
+        // 무명 메서드를 활용한 이벤트 연결 방식
+        optionButton.onClick.AddListener(delegate { OnButtonClick(optionButton.name); });
+
+        // 람다식을 활용한 이벤트 연결 방식
+        shopButton.onClick.AddListener(() => OnButtonClick(shopButton.name));
+    }
+
+    public void OnButtonClick(string msg)
+    {
+        Debug.Log($"Click Button : {msg}");
     }
 }
