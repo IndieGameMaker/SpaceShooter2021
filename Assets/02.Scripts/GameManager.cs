d40778468e772546d90a432455b2d4b879115857
Lee Jae Hyun <myleje@gmail.com>
2021-06-04 08:59:56 +0900

    [스크립트 8-2] GameManager - List 데이터 타입 사용

--- Assets/02.Scripts/GameManager.cs
+++ Assets/02.Scripts/GameManager.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // 몬스터가 출현할 위치를 저장할 배열
-    public Transform[] points;
+    // 몬스터가 출현할 위치를 저장할 List 타입 변수
+    public List<Transform> points = new List<Transform>();
 
     void Start()
     {
@@ -13,6 +13,6 @@ public class GameManager : MonoBehaviour
         Transform spawnPointGroup = GameObject.Find("SpawnPointGroup")?.transform;
 
         // SpawnPointGroup 하위에 있는 모든 차일드 게임오브젝트의 Transform 컴포넌트 추출
-        points = spawnPointGroup?.GetComponentsInChildren<Transform>();
+        spawnPointGroup?.GetComponentsInChildren<Transform>(points);
     }
 }
