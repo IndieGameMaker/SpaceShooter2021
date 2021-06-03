1d54c93f4942cd8a568c86e22a10534842b417ee
Lee Jae Hyun <myleje@gmail.com>
2021-06-04 08:57:00 +0900

    [스크립트 8-1] GameManager - SpawnPointGroup의 하위 컴포넌트 추출

---
+++ Assets/02.Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // 몬스터가 출현할 위치를 저장할 배열
+    public Transform[] points;
+
+    void Start()
+    {
+        // SpawnPointGroup 게임오브젝트의 Transform 컴포넌트 추출
+        Transform spawnPointGroup = GameObject.Find("SpawnPointGroup")?.transform;
+
+        // SpawnPointGroup 하위에 있는 모든 차일드 게임오브젝트의 Transform 컴포넌트 추출
+        points = spawnPointGroup?.GetComponentsInChildren<Transform>();
+    }
+}
