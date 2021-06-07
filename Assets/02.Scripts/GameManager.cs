16b635fdbc7673bfe0124261b544e900228d0557
Lee Jae Hyun <myleje@gmail.com>
2021-06-07 10:19:28 +0900

    [스크립트 8-12] GameManager - PlayerPrefs를 사용해 데이터 저장

--- Assets/02.Scripts/GameManager.cs
+++ Assets/02.Scripts/GameManager.cs
@@ -84,6 +84,7 @@ public class GameManager : MonoBehaviour
         InvokeRepeating("CreateMonster", 2.0f, createTime);
 
         // 스코어 점수 출력
+        totScore = PlayerPrefs.GetInt("TOT_SCORE", 0);
         DisplayScore(0);
     }
 
@@ -143,5 +144,7 @@ public class GameManager : MonoBehaviour
     {
         totScore += score;
         scoreText.text = $"<color=#00ff00>SCORE :</color> <color=#ff0000>{totScore:#,##0}</color>";
+        // 스코어 저장
+        PlayerPrefs.SetInt("TOT_SCORE", totScore);
     }
 }
